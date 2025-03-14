a734ccf83681fc2a2d61d615e55de76c965574a1
Talha <hpstar679@gmail.com>
2025-03-14 21:54:00 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  assetPrefix: './', // Fixes CSS/JS loading issues
-  images: { unoptimized: true }
 };
 
 export default nextConfig;
